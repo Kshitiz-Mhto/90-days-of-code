429ff36f123e83719390740b277df321fa58f2b0
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2022-10-16 23:47:24 +0545

    spring is update

--- SpringBoot/projpa/src/main/java/com/jpa/projpa/dao/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.jpa.projpa.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jpa.projpa.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-    
-}
